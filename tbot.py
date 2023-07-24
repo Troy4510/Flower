13dbde709706554d423c3d5ba5088579bdf9fb44
Troy4510 <88851318+Troy4510@users.noreply.github.com>
2023-07-24 19:28:23 +0200

    Add files via upload

--- tbot.py
+++ tbot.py
@@ -1,25 +1,26 @@
 import urllib.request
 import telebot
 from telebot import types
-bot1 = telebot.TeleBot ('---');
+import pygame
+import pygame.camera
+
+bot1 = telebot.TeleBot ('2108304241:AAFgaJLGr9pNFtdyW385ccIV0fG_c6c5PVY');
 admlist1 = ('troy4510', 'babayaga', 'dedmoroz')
 
 main1 = types.ReplyKeyboardMarkup(row_width=1,resize_keyboard=True)
 itembtn1 = types.KeyboardButton('Адрес сервера')
-itembtn2 = types.KeyboardButton('Статистика за день')
-itembtn3 = types.KeyboardButton('Кнопка')
-
+itembtn2 = types.KeyboardButton('Текущий статус')
+itembtn3 = types.KeyboardButton('Получить фото')
 
 main1.add(itembtn1, itembtn2, itembtn3)
 
-print ('БОТ ЗАПУЩЕН...')
+print ('[start telebot]')
 
 def msg(msg1):
     print (msg1)
 
 def get_ext_ip():
     external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-    #print(external_ip)
     return external_ip
 
 @bot1.message_handler(commands=['start'])
@@ -33,20 +34,36 @@ def get1_text_messages(message):
         ip1 = get_ext_ip()
         ans1 = str('http://' + ip1 + ':5000')
         bot1.send_message(message.from_user.id, ans1)
-        msg(f'запрос адреса от {message.from_user.first_name}')
+        msg(f'[request server from] {message.from_user.first_name}')
 
-    elif message.text == "Статистика за день":
-        ip1 = get_ext_ip()
-        ans1 = str('http://' + ip1 + ':5000/xomka')
+    elif message.text == "Текущий статус":
+        ans1 = str('в разработке')
         bot1.send_message(message.from_user.id, ans1)
 
-    elif message.text == "Кнопка":
-        bot1.send_message(message.from_user.id, "Динь-динь")
+    elif message.text == "Получить фото":
+        #bot1.send_message(message.from_user.id, "обработка запроса...")
+        try:
+            pygame.camera.init()
+            camlist = pygame.camera.list_cameras()
+            bot1.send_message(message.from_user.id, f"камеры: {camlist}")
+            if camlist:
+                cam = pygame.camera.Camera(camlist[0], (640, 480))
+                cam.start()
+                for i in range(10):
+                    image = cam.get_image()
+                #ready = pygame.camera.Camera.query_image()
+                #print(f'ready?: {ready}')
+                pygame.image.save(image, "cam01.jpg")
+                bot1.send_photo(message.chat.id, open('cam01.jpg', 'rb'))
+                cam.stop()
+        except:
+            bot1.send_message(message.from_user.id, "камера не отвечает")
 
     else:
-        bot1.send_message(message.from_user.id, "Моя твоя не понимать")
+        bot1.send_message(message.from_user.id, "Неизвестная команда")
         msgstr1 = message.from_user.first_name + " say: " + message.text
         msg(str(msgstr1))
 
 if __name__ == '__main__':
+    msg('[bot is online]')
     bot1.polling(none_stop=True, interval=5)
